e03400b2628d271ebb0ae882de6884b0f29032d2
vikash-mahar <vmahar808@gmail.com>
2026-04-23 12:04:13 +0530

    api changes

--- client/src/api.js
+++ client/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   // baseURL: "http://localhost:5001/api"
-  baseURL:"https://taskmanagerwithauths.onrender.com"
+  baseURL:"https://taskmanagerwithauths.onrender.com/api"
 });
 
 export default API;
